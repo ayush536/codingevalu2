134a5c420725375976fea3beb72bcf9da00f4cb9
Pratik Singh <singhpratik26297@gmail.com>
2022-03-24 18:05:24 +0530

    newone

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import { AddHouse } from "./Components/AddHouse/AddHouse";
 function App() {
   return (
     <div className="App">
-      <Rentals />
-      <button className="toggleForm">
+      <Rentals/>
+      <button className="toggleForm">hii
         {/* Show text Add House or Show Rentals based on state */}
       </button>
       <AddHouse/>
-      
       {/* Show component based on state */}
       <br />
     </div>
